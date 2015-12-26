de673e522b564bc9b5a79986a47012b36b9e967b
tradaduong2811 <htluan2811@gmail.com>
2015-12-26 12:29:53 +0700

    update dataabase

--- PhuongNam_Data/Program.cs
+++ PhuongNam_Data/Program.cs
@@ -16,7 +16,7 @@ namespace PhuongNam_Data
             DataTable dt = qg.displayCountry();
             foreach (DataRow item in dt.Rows)
 	        {
-                Console.WriteLine(item["MaDDH"]);
+                Console.WriteLine(item["maDDH"]);
 	        }
         }
     }
