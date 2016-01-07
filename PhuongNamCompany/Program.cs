0642c9185c8a85f470da59a48959bc05a56c04fa
Nam Pham <nampham7394@gmail.com>
2016-01-07 09:44:18 +0700

    Fix Conflict

--- PhuongNamCompany/Program.cs
+++ PhuongNamCompany/Program.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,27 +21,4 @@ namespace PhuongNamCompany
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace PhuongNamCompany
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MH_BoPhanMuaHang());
-        }
-    }
-}
->>>>>>> 6b162ad6c24788ff792a79b5497b304506b2819c
